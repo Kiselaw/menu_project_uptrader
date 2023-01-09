3449b0061f13f620300649e131dc71aad4435f04
Kiselaw <ProstoCOS@gmail.com>
2023-01-09 23:22:58 +0300

    Removed redundant comments

--- menu_project/menu/templatetags/templatetags.py
+++ menu_project/menu/templatetags/templatetags.py
@@ -8,9 +8,4 @@ register = template.Library()
 @register.inclusion_tag('menu.html')
 def show_results(title):
     menu = Menu.objects.get(menu_title=title)
-    print(menu.items)
-    # for item in menu_items:
-    #     a = item.subitems.all()
-    #     print(a)
-    # print(menu_items)
     return {'menu': menu}
